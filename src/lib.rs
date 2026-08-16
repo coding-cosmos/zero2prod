549309886665e506cbf208ee52d80ea94bba2a0f
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2026-08-16 18:29:48 +0530

    Add listener argument

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! lib.rs
 
+use std::net::TcpListener;
+
 use actix_web::{App, HttpResponse, HttpServer, dev::Server, web};
 
 async fn health_check() -> HttpResponse {
@@ -8,9 +10,9 @@ async fn health_check() -> HttpResponse {
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro incantation.
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
+        .listen(listener)?
         .run();
 
     Ok(server)
