3337c0ffb018e9bfc5e2f4d7dacd1341831d4f72
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2026-08-26 18:31:34 +0530

    Add domain validation

---
+++ src/domain/new_subscriber.rs
@@ -0,0 +1,5 @@
+use crate::domain::{subscriber_email::SubscriberEmail, subscriber_name::SubscriberName};
+pub struct NewSubscriber {
+    pub email: SubscriberEmail,
+    pub name: SubscriberName,
+}
