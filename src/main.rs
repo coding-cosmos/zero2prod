b984b982ffde29cc6f7898d286d04f47f0199024
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2026-08-27 15:57:39 +0530

    Add email client in app

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use sqlx::{
 use std::net::TcpListener;
 use zero2prod::{
     configuration::get_configuration,
+    email_client::EmailClient,
     startup::run,
     telemetry::{get_subscriber, init_subscriber},
 };
@@ -26,10 +27,23 @@ async fn main() -> std::io::Result<()> {
         .ssl_mode(PgSslMode::Disable);
 
     let connection_pool = PgPool::connect_lazy_with(database_options);
+
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address");
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout
+    );
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await
 }
