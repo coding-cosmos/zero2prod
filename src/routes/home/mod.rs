426086d8827df704537e51a5e6a9e5b4cfbe0488
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2026-09-15 22:01:30 +0530

    Add home

---
+++ src/routes/home/mod.rs
@@ -0,0 +1,7 @@
+//! src/routes/home/mod.rs
+use actix_web::{HttpResponse, http::header::ContentType};
+pub async fn home() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(include_str!("home.html"))
+}
