448651a44c69b4b746e763b7b528478ef4d1321b
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2026-09-15 22:02:18 +0530

    add bew modules

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -3,8 +3,12 @@ pub(crate) mod health_check;
 mod newsletter;
 pub(crate) mod subscriptions;
 mod subscriptions_confirm;
+mod home;
+mod login;
 
 pub use health_check::*;
 pub use newsletter::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
+pub use home::*;
+pub use login::*;
