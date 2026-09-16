4cc606fc97b0a498eacc83d4730387e65530d69e
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2026-09-16 23:39:15 +0530

    Add admin module

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -5,6 +5,7 @@ pub(crate) mod subscriptions;
 mod subscriptions_confirm;
 mod home;
 mod login;
+mod admin;
 
 pub use health_check::*;
 pub use newsletter::*;
@@ -12,3 +13,4 @@ pub use subscriptions::*;
 pub use subscriptions_confirm::*;
 pub use home::*;
 pub use login::*;
+pub use admin::*;
